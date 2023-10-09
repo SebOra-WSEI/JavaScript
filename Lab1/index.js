dbe540805b437c3635749a2c7f8f67fb8976b2ed
SebOra-WSEI <LiBroPWSZ@gmail.com>
2023-10-09 17:37:19 +0200

    Second_point

--- Lab1/index.js
+++ Lab1/index.js
@@ -2,22 +2,37 @@ const btn = document.querySelector("button");
 
 btn.addEventListener("click", function (event) {
   event.preventDefault();
+  mainLogic();
+});
 
+const mainLogic = () => {
   const inputs = document.querySelectorAll("input");
+  const valuesBox = document.querySelector("#valuesBox");
   const displayedValues = document.querySelector("#displayedValues");
+  valuesBox.removeChild(displayedValues);
 
   let values = mapValues(inputs);
 
-  createElement(displayedValues, "SUM", getSum(values));
-  createElement(displayedValues, "AVG", getAverage(values));
-  createElement(displayedValues, "MIN", Math.min(...values));
-  createElement(displayedValues, "MAX", Math.max(...values));
-});
+  createElement(valuesBox, "SUM", getSum(values));
+  createElement(valuesBox, "AVG", getAverage(values));
+  createElement(valuesBox, "MIN", Math.min(...values));
+  createElement(valuesBox, "MAX", Math.max(...values));
+};
+
+const createElement = (valuesBox, operation, value) => {
+  const element = document.createElement("p");
+  element.textContent = `${operation}: ${value}`;
+  valuesBox.appendChild(element);
+};
 
 const getSum = (inputs) => {
   let sum = 0;
 
-  inputs.forEach((input) => (sum += input));
+  inputs.forEach((input) => {
+    if (input) {
+      sum += input;
+    }
+  });
 
   return sum;
 };
@@ -25,13 +40,15 @@ const getSum = (inputs) => {
 const getAverage = (inputs) => getSum(inputs) / inputs.length;
 
 const mapValues = (inputs) => {
-  let values = [];
-  inputs.forEach((input) => values.push(parseInt(input.value)));
-  return values;
-};
+  let values = Array(inputs.length).fill(0);
 
-const createElement = (displayedValues, operation, value) => {
-  const element = document.createElement("p");
-  element.textContent = `${operation}: ${value}`;
-  displayedValues.appendChild(element);
+  inputs.forEach((input, index) => {
+    const value = parseInt(input.value);
+
+    if (value) {
+      values[index] = value;
+    }
+  });
+
+  return values;
 };
